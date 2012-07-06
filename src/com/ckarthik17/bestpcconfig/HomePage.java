6f70b4bf50e1773def9544e909df95c457575f11
Karthik Chandraraj <ckarthik17@gmail.com>
2012-07-06 22:37:48 +0530

    Initial project structure

---
+++ src/com/ckarthik17/bestpcconfig/HomePage.java
@@ -0,0 +1,20 @@
+package com.ckarthik17.bestpcconfig;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class HomePage extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void sendMessage(View view) {
+
+    }
+}
