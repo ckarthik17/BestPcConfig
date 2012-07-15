28813daae9872bfd7c9733828855ff5128928128
Karthik Chandraraj <ckarthik17@gmail.com>
2012-07-15 21:41:05 +0530

    Http call to retrieve blog content

--- src/com/ckarthik17/bestpcconfig/HomePage.java
+++ src/com/ckarthik17/bestpcconfig/HomePage.java
@@ -1,27 +1,85 @@
 package com.ckarthik17.bestpcconfig;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class HomePage extends Activity
 {
-    /** Called when the activity is first created. */
+    public static final String BLOGGER_API_KEY_PROPERTY = "BLOGGER_API_KEY";
+    public static String BLOGGER_API_KEY;
+    private final String LOG_TAG = "BEST PC CONFIG : ";
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        try {
+            Resources resources = this.getResources();
+            AssetManager assetManager = resources.getAssets();
+
+            InputStream inputStream = assetManager.open("config.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+
+            BLOGGER_API_KEY = (String) properties.get(BLOGGER_API_KEY_PROPERTY);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editTextBox = (EditText) findViewById(R.id.edit_message);
-        String value = editTextBox.getText().toString();
-        intent.putExtra(DisplayMessageActivity.TEXT_VALUE, value);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        startActivity(intent);
+        Log.i(LOG_TAG, "Making HTTP call...");
+        RetrieveBlogContent retrieveBlogContent = new RetrieveBlogContent();
+        AsyncTask<Void, Void, String> executionResult = retrieveBlogContent.execute();
+        try {
+            Log.i(LOG_TAG, executionResult.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class RetrieveBlogContent extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet("https://www.googleapis.com/blogger/v3/blogs/230842735819274721/posts/1186418786118781314?key=" + BLOGGER_API_KEY);
+            String text = null;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                text = EntityUtils.toString(entity);
+                Log.i(LOG_TAG, text);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return text;
+        }
     }
 }
